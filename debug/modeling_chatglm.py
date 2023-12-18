14635d82121df42dbd53d974be5b8e0cde34af63
vision1v1 <vision1v1@163.com>
2023-12-18 18:21:58 +0800

    优化方便调试

--- debug/modeling_chatglm.py
+++ debug/modeling_chatglm.py
@@ -25,7 +25,7 @@ from transformers.utils import logging
 from transformers.generation.logits_process import LogitsProcessor
 from transformers.generation.utils import LogitsProcessorList, StoppingCriteriaList, GenerationConfig, ModelOutput
 
-from .configuration_chatglm import ChatGLMConfig
+from configuration_chatglm import ChatGLMConfig
 
 # flags required to enable jit fusion kernels
 
